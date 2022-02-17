9549b13e8c6580d066d88be89996baff02263691
florianzl <f.zitlau@web.de>
2022-02-17 01:01:30 +0100

    added navigationBar

---
+++ app.js
@@ -0,0 +1,27 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navLinks');
+    const navLinks = document.querySelectorAll('.navLinks li')
+
+    
+    burger.addEventListener('click', () => {
+        //Togle Nav
+        nav.classList.toggle('nav-active');
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s';
+            }
+        });
+        //Burger animation
+        burger.classList.toggle('toggle');
+    });
+
+    
+    
+}
+
+navSlide();
