2e77c31f44bb3b340fa0240f47eb1475b73f945b
florianzl <f.zitlau@web.de>
2022-02-17 21:14:00 +0100

    good responsive design for mobile

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.navLinks');
-    const navLinks = document.querySelectorAll('.navLinks li')
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li')
 
     
     burger.addEventListener('click', () => {
-        //Togle Nav
+        //Togle Nav 
         nav.classList.toggle('nav-active');
 
         //Animate Links
@@ -13,7 +13,7 @@ const navSlide = () => {
             if (link.style.animation) {
                 link.style.animation = '';
             } else {
-                link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s';
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
             }
         });
         //Burger animation
