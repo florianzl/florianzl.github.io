f6627187d5caf2a8e291ba3077b547224e36fb3c
florianzl <f.zitlau@web.de>
2022-06-14 16:35:09 +0200

    fix navbar mobile

--- app.js
+++ app.js
@@ -19,6 +19,22 @@ const navSlide = () => {
         //Burger animation
         burger.classList.toggle('toggle');
     });
+
+    nav.addEventListener('click', () => {
+        //Togle Nav 
+        nav.classList.toggle('nav-active');
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+        //Burger animation
+        burger.classList.toggle('toggle');
+    });
 }
 
 navSlide();
